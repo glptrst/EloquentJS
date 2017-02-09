3814e0a2557a8f338e7e8dcdb10a18d83d47e269
Giulio <glptrst@users.noreply.github.com>
2017-02-09 16:11:10 +0100

    Update loopingATriangle.js

--- chap2/loopingATriangle.js
+++ chap2/loopingATriangle.js
@@ -12,3 +12,11 @@ while (string.length < 7) {
 	string += "#";
 	console.log(string);
 }
+
+//without global variable:
+for (var i = 0; i < 7; i++) {
+  	var string = "";
+  	for (var j = 0; j < i; j++)
+    		string += "#";
+  	console.log(string);
+}
